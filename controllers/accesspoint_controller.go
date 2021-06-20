5284fbc5e410e639953dc2877ef3ac15499a7d4f
RyuSA <kommsussertad@gmail.com>
2021-06-20 23:57:57 +0900

    add sample HostapdConfiguration

--- controllers/accesspoint_controller.go
+++ controllers/accesspoint_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"strings"
 
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -117,4 +118,50 @@ func (r *AccessPointReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&accesspointv1alpha1.AccessPoint{}).
 		Complete(r)
+}
+
+type HostapdConfiguration struct {
+	networkinterface string
+	ssid             string
+	password         string
+}
+
+func NewHostapdConfiguration(accesspoint *accesspointv1alpha1.AccessPoint) *HostapdConfiguration {
+	return &HostapdConfiguration{
+		networkinterface: accesspoint.Spec.Interface,
+		ssid:             accesspoint.Spec.Ssid,
+		password:         accesspoint.Spec.Password,
+	}
+}
+
+func (h *HostapdConfiguration) Configure() string {
+	defaultconfig := `
+interface=wlan0
+driver=nl80211
+bridge=br0
+hw_mode=g
+channel=6
+ieee80211d=1
+country_code=JP
+ieee80211n=1
+wmm_enabled=1
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=password
+wpa_key_mgmt=WPA-PSK
+rsn_pairwise=CCMP
+ctrl_interface=/var/run/hostapd
+ctrl_interface_group=0
+`
+	var builder strings.Builder
+	join := func(key string, value string) string {
+		return key + "=" + value + "\n"
+	}
+	builder.WriteString(defaultconfig)
+	builder.WriteString(join("interface", h.networkinterface))
+	builder.WriteString(join("ssid", h.ssid))
+	builder.WriteString(join("wpa_passphrase", h.password))
+	return builder.String()
 }
