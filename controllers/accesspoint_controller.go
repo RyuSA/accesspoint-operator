7287ba71feed3b198b587ed097e438f66f6e997f
RyuSA <kommsussertad@gmail.com>
2021-06-21 22:48:00 +0900

    it "works"
    need to add some error handling and so on...

--- controllers/accesspoint_controller.go
+++ controllers/accesspoint_controller.go
@@ -18,18 +18,25 @@ package controllers
 
 import (
 	"context"
-	"strings"
+	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	hostapd "github.com/RyuSA/accesspoint-operator/internal/hostapd"
 
 	accesspointv1alpha1 "github.com/RyuSA/accesspoint-operator/api/v1alpha1"
 	"github.com/go-logr/logr"
+)
+
+var (
+	controllerOwnerKey = ".metadata.controller"
+	apiGVStr           = accesspointv1alpha1.GroupVersion.String()
 )
 
 // AccessPointReconciler reconciles a AccessPoint object
@@ -62,106 +69,181 @@ func (r *AccessPointReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	log := r.Log.WithValues("accesspoint", req.NamespacedName)
 	log.Info("start reconsile")
 
+	accesspoint_base_label := map[string]string{
+		"app.kubernetes.io/name":     "accesspoint",
+		"app.kubernetes.io/instance": req.Name,
+	}
+
 	// リクエスト内容を元にAPを検索します
 	// このAPと1対1に紐つくDaemonSetを作成していきます
 	var accesspoint accesspointv1alpha1.AccessPoint
 	log.Info("fetching AccessPoint named", "name", req.NamespacedName)
 	if err := r.Get(ctx, req.NamespacedName, &accesspoint); err != nil {
-		// リソースが存在しない場合をここでキャプチャしています
-		// リソースが存在しない = アクセスポイント設定がない ことを表現しているので対応するDaemonSetを削除します
-		if apierrors.IsNotFound(err) {
-			log.Info("The AP has been deleted. Deleting related DaemonSet...", "AP name", req.NamespacedName)
-			// TODO 一致するDaemonSetを削除します
-			return ctrl.Result{}, nil
-		}
-		log.Error(err, "failed to fetching Accesspoint")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-	log.Info("captured AccesspointDevice", "accesspoint", accesspoint)
+	log.Info("Accesspoint has been successfully fetched")
+	hostapdConfig := NewHostapdConfiguration(&accesspoint)
 
 	// APの組み合わせに紐つくDaemonSetを取得します
 	// 存在していれば変更点の調査、更新します
 	// 存在していなければ新規作成します
+	log.Info("fetching related DaemonSets...")
 	var daemonsets appsv1.DaemonSetList
-	if err := r.List(ctx, &daemonsets, client.MatchingLabels{
-		"app.kubernetes.io/name":     "accesspoint",
-		"app.kubernetes.io/instance": req.NamespacedName.String(),
-	}); err != nil {
-		log.Error(err, "failed to fetch daemonset")
+	if err := r.List(ctx, &daemonsets, client.MatchingLabels(accesspoint_base_label)); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-
-	log.Info("captured DaemonSet", "daemonset-list", daemonsets)
+	log.Info("DaemonSets has been successfully fetched")
 
 	// APに紐つくDaemonSetの個数
 	numberOfDaemonSetRelatedToAp := len(daemonsets.Items)
 
-	// TODO
-	switch numberOfDaemonSetRelatedToAp {
-	case 0:
-		log.Info("create new DaemonSet!")
-		// ConfigMapを作成します
-		// DaemonSetを作成します
-	case 1:
-		log.Info("update the DaemonSet", "daemonset", daemonsets.Items[len(daemonsets.Items)-1])
-		// ctrl.CreateOrUpdateでConfigMapを更新します
-		// ctrl.CreateOrUpdateでDaemonSetを更新します
-	default:
+	createLabel := func(label map[string]string, instance string) map[string]string {
+		if label == nil {
+			label = make(map[string]string)
+		}
+		label["app.kubernetes.io/name"] = "accesspoint"
+		label["app.kubernetes.io/instance"] = instance
+		return label
+	}
+
+	// TODO ましな実装にする
+	if numberOfDaemonSetRelatedToAp == 0 || numberOfDaemonSetRelatedToAp == 1 {
+		log.Info("creating configmap...")
+		configmap := &corev1.ConfigMap{}
+		configmap.SetName(req.Name)
+		configmap.SetNamespace(req.Namespace)
+		if _, err := ctrl.CreateOrUpdate(ctx, r.Client, configmap, func() error {
+			configmap.Labels = createLabel(configmap.Labels, req.Name)
+			configmap.Data = make(map[string]string)
+			configmap.Data["hostapd.conf"] = hostapdConfig.Configure()
+			if err := ctrl.SetControllerReference(&accesspoint, configmap, r.Scheme); err != nil {
+				log.Error(err, "unable to set ownerReference")
+				return err
+			}
+			return nil
+		}); err != nil {
+			log.Error(err, "Error during creating configmap...")
+			return ctrl.Result{
+				RequeueAfter: time.Minute * 1,
+			}, err
+		}
+		log.Info("configmap has been successfully created")
+
+		log.Info("creating daemonset...")
+		daemonset := &appsv1.DaemonSet{}
+		daemonset.SetName(req.Name)
+		daemonset.SetNamespace(req.Namespace)
+		if _, err := ctrl.CreateOrUpdate(ctx, r.Client, daemonset, func() error {
+			daemonset.Labels = createLabel(daemonset.Labels, req.Name)
+			daemonset.Spec = appsv1.DaemonSetSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: accesspoint_base_label,
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: accesspoint_base_label,
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Image: "nginx:latest",
+								Name:  "nginx",
+								VolumeMounts: []corev1.VolumeMount{
+									{
+										Name:      "config",
+										MountPath: "/hostapd/conf/",
+									},
+								},
+							},
+						},
+						Volumes: []corev1.Volume{
+							{
+								Name: "config",
+								VolumeSource: corev1.VolumeSource{
+									ConfigMap: &corev1.ConfigMapVolumeSource{
+										LocalObjectReference: corev1.LocalObjectReference{
+											Name: req.Name,
+										},
+									},
+								},
+							},
+						},
+						NodeSelector: accesspoint.Spec.NodeSelector,
+					},
+				},
+			}
+
+			if err := ctrl.SetControllerReference(&accesspoint, daemonset, r.Scheme); err != nil {
+				log.Error(err, "unable to set ownerReference")
+				return err
+			}
+			return nil
+		}); err != nil {
+			log.Error(err, "Error during creating daemonset...")
+			return ctrl.Result{
+				RequeueAfter: time.Minute * 1,
+			}, err
+		}
+
+		log.Info("daemonset has been successfully created")
+	} else {
 		log.Info("there are multiple daemonset related to this event!", "namespacedname", req.NamespacedName, "daemonsets", daemonsets)
 	}
 
+	log.Info("sync done")
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *AccessPointReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	if err := mgr.GetFieldIndexer().IndexField(
+		context.Background(),
+		&appsv1.DaemonSet{},
+		controllerOwnerKey,
+		func(raw client.Object) []string {
+			daemonset := raw.(*appsv1.DaemonSet)
+			owner := metav1.GetControllerOf(daemonset)
+			if owner == nil {
+				return nil
+			}
+			if owner.APIVersion != apiGVStr || owner.Kind != "AccessPoint" {
+				return nil
+			}
+			return []string{owner.Name}
+		}); err != nil {
+		return err
+	}
+
+	if err := mgr.GetFieldIndexer().IndexField(
+		context.Background(),
+		&corev1.ConfigMap{},
+		controllerOwnerKey,
+		func(raw client.Object) []string {
+			configmap := raw.(*corev1.ConfigMap)
+			owner := metav1.GetControllerOf(configmap)
+			if owner == nil {
+				return nil
+			}
+			if owner.APIVersion != apiGVStr || owner.Kind != "AccessPoint" {
+				return nil
+			}
+			return []string{owner.Name}
+		}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&accesspointv1alpha1.AccessPoint{}).
+		Owns(&appsv1.DaemonSet{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
 
-type HostapdConfiguration struct {
-	networkinterface string
-	ssid             string
-	password         string
-}
-
-func NewHostapdConfiguration(accesspoint *accesspointv1alpha1.AccessPoint) *HostapdConfiguration {
-	return &HostapdConfiguration{
-		networkinterface: accesspoint.Spec.Interface,
-		ssid:             accesspoint.Spec.Ssid,
-		password:         accesspoint.Spec.Password,
+func NewHostapdConfiguration(accesspoint *accesspointv1alpha1.AccessPoint) *hostapd.HostapdConfiguration {
+	return &hostapd.HostapdConfiguration{
+		Networkinterface: accesspoint.Spec.Interface,
+		Ssid:             accesspoint.Spec.Ssid,
+		Password:         accesspoint.Spec.Password,
 	}
-}
-
-func (h *HostapdConfiguration) Configure() string {
-	defaultconfig := `
-interface=wlan0
-driver=nl80211
-bridge=br0
-hw_mode=g
-channel=6
-ieee80211d=1
-country_code=JP
-ieee80211n=1
-wmm_enabled=1
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=password
-wpa_key_mgmt=WPA-PSK
-rsn_pairwise=CCMP
-ctrl_interface=/var/run/hostapd
-ctrl_interface_group=0
-`
-	var builder strings.Builder
-	join := func(key string, value string) string {
-		return key + "=" + value + "\n"
-	}
-	builder.WriteString(defaultconfig)
-	builder.WriteString(join("interface", h.networkinterface))
-	builder.WriteString(join("ssid", h.ssid))
-	builder.WriteString(join("wpa_passphrase", h.password))
-	return builder.String()
 }
